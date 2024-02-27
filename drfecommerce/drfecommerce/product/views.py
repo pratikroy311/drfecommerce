f2c65ded82f7ce2d08886b290992f31451e2e6d5
pratik <pratikroy311@gmail.com>
2024-02-27 13:40:16 +0530

    working on viewset

--- drfecommerce/drfecommerce/product/views.py
+++ drfecommerce/drfecommerce/product/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from rest_framework.response import Response
+from rest_framework import viewsets
+from .models import *
+from .serializers import *
 
-# Create your views here.
+class CategoryView(viewsets.ViewSet):
+    queryset = Category.objects.all()
+    
+    def list(self,request):
+        serializer = CategorySerializer(self.queryset,many=True)
+        return Response(serializer.data)
