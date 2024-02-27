15f9c64ac627b944bcd68dcc417bdef7812dfcc0
pratik <pratikroy311@gmail.com>
2024-02-27 19:59:56 +0530

    creating serializer,router for product app

--- drfecommerce/drfecommerce/settings/base.py
+++ drfecommerce/drfecommerce/settings/base.py
@@ -23,6 +23,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'drf_spectacular',
     'drfecommerce.product',
 ]
 
@@ -100,4 +101,10 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
-REST_FRAMEWORK = {}
+REST_FRAMEWORK = {
+    'DEFAULT_SCHEMA_CLASS': 'drf_spectacular.openapi.AutoSchema',
+}
+
+SPECTACULAR_SETTINGS= {
+    "TITLE": "DJANGO DRF Ecommerce",
+}
--- drfecommerce/drfecommerce/urls.py
+++ drfecommerce/drfecommerce/urls.py
@@ -2,12 +2,16 @@ from django.contrib import admin
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from drfecommerce.product import views
-
+from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
 
 router = DefaultRouter()
 router.register(r'category',views.CategoryViewSet)
+router.register(r'brand',views.BrandViewSet)
+router.register(r'product',views.ProductViewSet)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/',include(router.urls))
+    path('api/',include(router.urls)),
+    path("api/Schema/",SpectacularAPIView.as_view(),name="schema"),
+    path("api/schema/docs/",SpectacularSwaggerView.as_view(url_name="schema")),
 ]
